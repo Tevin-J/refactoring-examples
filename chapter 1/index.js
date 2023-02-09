e69d5d5c38dacd087a490b17ff0246177fd21ae7
Anton Revta <anton.revta@gmail.com>
2023-02-09 11:39:24 +0300

    use play inline variable in amount for

--- chapter 1/index.js
+++ chapter 1/index.js
@@ -32,24 +32,24 @@ playFor = (performance) => {
   return plays[performance.playID];
 };
 
-amountFor = (perf, play) => {
+amountFor = (performance, play) => {
   let result = 0;
-  switch (play.type) {
+  switch (playFor(performance).type) {
     case 'tragedy':
       result = 40000;
-      if (perf.audience > 30) {
-        result += 1000 * (perf.audience - 30);
+      if (performance.audience > 30) {
+        result += 1000 * (performance.audience - 30);
       }
       break;
     case 'comedy':
       result = 30000;
-      if (perf.audience > 20) {
-        result += 1000 + 500 * (perf.audience - 20);
+      if (performance.audience > 20) {
+        result += 1000 + 500 * (performance.audience - 20);
       }
-      result += 300 * perf.audience;
+      result += 300 * performance.audience;
       break;
     default:
-      throw new Error(`unknown type: ${play.type}`);
+      throw new Error(`unknown type: ${playFor(performance).type}`);
   }
   return result;
 };
