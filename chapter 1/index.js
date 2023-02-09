9be95e323ab08b7074e86b017dcfe1e9f862531b
Anton Revta <anton.revta@gmail.com>
2023-02-09 10:59:18 +0300

    extract amountFor function

--- chapter 1/index.js
+++ chapter 1/index.js
@@ -1,7 +1,7 @@
 const invoices = require('./invoices.json');
 const plays = require('./plays.json');
 
-function statement(invoice, plays) {
+statement = (invoice, plays) => {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
@@ -13,26 +13,7 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 1000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
-
+    const thisAmount = amountFor(perf, play);
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
@@ -46,7 +27,29 @@ function statement(invoice, plays) {
   result += `You earned ${volumeCredits} credits\n`;
   console.log(result);
   return result;
-}
+};
+
+amountFor = (perf, play) => {
+  let thisAmount = 0;
+  switch (play.type) {
+    case 'tragedy':
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case 'comedy':
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 1000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+  return thisAmount;
+};
 
 exports.chapter1 = () => {
   statement(invoices[0], plays);
