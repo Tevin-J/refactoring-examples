21c2e4702f1538e0da401b717e4813279695793d
Anton Revta <anton.revta@gmail.com>
2023-02-09 11:41:05 +0300

    remove play argument from amountFor

--- chapter 1/index.js
+++ chapter 1/index.js
@@ -12,7 +12,7 @@ statement = (invoice, plays) => {
   }).format;
 
   for (let perf of invoice.performances) {
-    const thisAmount = amountFor(perf, playFor(perf));
+    const thisAmount = amountFor(perf);
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
@@ -32,7 +32,7 @@ playFor = (performance) => {
   return plays[performance.playID];
 };
 
-amountFor = (performance, play) => {
+amountFor = (performance) => {
   let result = 0;
   switch (playFor(performance).type) {
     case 'tragedy':
