6677762634386b01f5acd8b8c2c128cc65803625
Anton Revta <anton.revta@gmail.com>
2023-02-10 23:38:47 +0300

    move amount in intermediate data

--- chapter 1/index.js
+++ chapter 1/index.js
@@ -12,6 +12,29 @@ const statement = (invoice, plays) => {
 function enrichPerformance(performance) {
   const result = Object.assign({}, performance);
   result.play = ployFor(result);
+  result.amount = amountFor(result);
+  return result;
+}
+
+function amountFor(performance) {
+  let result = 0;
+  switch (performance.play.type) {
+    case 'tragedy':
+      result = 40000;
+      if (performance.audience > 30) {
+        result += 1000 * (performance.audience - 30);
+      }
+      break;
+    case 'comedy':
+      result = 30000;
+      if (performance.audience > 20) {
+        result += 1000 + 500 * (performance.audience - 20);
+      }
+      result += 300 * performance.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${performance.play.type}`);
+  }
   return result;
 }
 
@@ -24,7 +47,7 @@ function renderPlainText(data) {
 
   for (let perf of data.performances) {
     // print line for this order
-    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
   }
 
   result += `Amount owed is ${usd(totalAmount())}\n`;
@@ -44,7 +67,7 @@ function renderPlainText(data) {
     let result = 0;
     for (let perf of data.performances) {
       // print line for this order
-      result += amountFor(perf);
+      result += perf.amount;
     }
     return result;
   }
@@ -62,28 +85,6 @@ function renderPlainText(data) {
       currency: 'USD',
       minimumFractionDigits: 2,
     }).format(number / 100);
-  }
-
-  function amountFor(performance) {
-    let result = 0;
-    switch (performance.play.type) {
-      case 'tragedy':
-        result = 40000;
-        if (performance.audience > 30) {
-          result += 1000 * (performance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30000;
-        if (performance.audience > 20) {
-          result += 1000 + 500 * (performance.audience - 20);
-        }
-        result += 300 * performance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${performance.play.type}`);
-    }
-    return result;
   }
 }
 
