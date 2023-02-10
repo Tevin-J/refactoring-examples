922a601e5f5efebcc7a3c4bb31325948c9015082
Anton Revta <anton.revta@gmail.com>
2023-02-10 23:55:34 +0300

    extract calculation logic in separate function

--- chapter 1/index.js
+++ chapter 1/index.js
@@ -2,13 +2,17 @@ const invoices = require('./invoices.json');
 const plays = require('./plays.json');
 
 const statement = (invoice, plays) => {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return renderPlainText(statementData);
+  return renderPlainText(createStatementData(invoice, plays));
 };
+
+function createStatementData(invoice, plays) {
+  const result = {};
+  result.customer = invoice.customer;
+  result.performances = invoice.performances.map(enrichPerformance);
+  result.totalAmount = totalAmount(result);
+  result.totalVolumeCredits = totalVolumeCredits(result);
+  return result;
+}
 
 function enrichPerformance(performance) {
   const result = Object.assign({}, performance);
