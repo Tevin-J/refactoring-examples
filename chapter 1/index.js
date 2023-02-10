deea9dd5979d87ab4f2d94c58ba86d1dac234d4c
Anton Revta <anton.revta@gmail.com>
2023-02-10 21:48:59 +0300

    remove thisAmount to use inline variable

--- chapter 1/index.js
+++ chapter 1/index.js
@@ -12,15 +12,14 @@ const statement = (invoice, plays) => {
   }).format;
 
   for (let perf of invoice.performances) {
-    const thisAmount = amountFor(perf);
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
     if (playFor(perf).type === 'comedy') volumeCredits += Math.floor(perf.audience);
 
     // print line for this order
-    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
