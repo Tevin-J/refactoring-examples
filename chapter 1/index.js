6c995a71b9122a0f004b6e43436c61e4e5828410
Anton Revta <anton.revta@gmail.com>
2023-02-10 22:00:09 +0300

    add format divider in usd function

--- chapter 1/index.js
+++ chapter 1/index.js
@@ -10,10 +10,10 @@ const statement = (invoice, plays) => {
     volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   console.log(result);
   return result;
@@ -31,7 +31,7 @@ const usd = (number) => {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2,
-  }).format(number);
+  }).format(number / 100);
 };
 
 const playFor = (performance) => {
