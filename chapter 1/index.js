313c6dc1847280966a55237a251e4904f8d60c5a
Anton Revta <anton.revta@gmail.com>
2023-02-10 22:59:33 +0300

    move total amount to separate function. make random name to change it later with inline variable step

--- chapter 1/index.js
+++ chapter 1/index.js
@@ -2,14 +2,14 @@ const invoices = require('./invoices.json');
 const plays = require('./plays.json');
 
 const statement = (invoice, plays) => {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
     // print line for this order
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-    totalAmount += amountFor(perf);
   }
+
+  let totalAmount = appleSauce();
 
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
@@ -22,6 +22,15 @@ const statement = (invoice, plays) => {
       result += volumeCreditsFor(perf);
     }
     return result;
+  }
+
+  function appleSauce() {
+    let totalAmount = 0;
+    for (let perf of invoice.performances) {
+      // print line for this order
+      totalAmount += amountFor(perf);
+    }
+    return totalAmount;
   }
 
   function volumeCreditsFor(performance) {
