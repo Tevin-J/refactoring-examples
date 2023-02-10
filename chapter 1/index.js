192b618ddbb3a18290cf27f3135d70877593e7f2
Anton Revta <anton.revta@gmail.com>
2023-02-10 23:19:29 +0300

    create object that will be intermediate data structure between two parts

--- chapter 1/index.js
+++ chapter 1/index.js
@@ -2,10 +2,11 @@ const invoices = require('./invoices.json');
 const plays = require('./plays.json');
 
 const statement = (invoice, plays) => {
-  return renderPlainText(invoice, plays);
+  const statementData = {};
+  return renderPlainText(statementData, invoice, plays);
 };
 
-function renderPlainText(invoice, plays) {
+function renderPlainText(data, invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
