936e0a5c60be4d2b8e210346010e0dbbadd8819d
Anton Revta <anton.revta@gmail.com>
2023-02-12 16:56:43 +0300

    create function that will create different calculator classes based on play type

--- chapter 1/createStatementData.js
+++ chapter 1/createStatementData.js
@@ -9,12 +9,16 @@ exports.createStatementData = (invoice, plays) => {
   return result;
 
   function enrichPerformance(performance) {
-    const calculator = new PerformanceCalculator(performance, playFor(performance));
+    const calculator = createPerformanceCalculator(performance, playFor(performance));
     const result = Object.assign({}, performance);
     result.play = calculator.play;
     result.amount = calculator.amount;
     result.volumeCredits = calculator.volumeCredits;
     return result;
+  }
+
+  function createPerformanceCalculator(performance, play) {
+    return new PerformanceCalculator(performance, play);
   }
 
   function totalVolumeCredits(data) {
