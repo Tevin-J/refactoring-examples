f49d6ea8d89be73b5ddea6ac387be99b5838454c
Anton Revta <anton.revta@gmail.com>
2023-02-12 17:11:25 +0300

    add volume credits getter for comedy calculator

--- chapter 1/performanceCalculator.js
+++ chapter 1/performanceCalculator.js
@@ -9,10 +9,7 @@ class PerformanceCalculator {
   }
 
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
-    if (this.play.type === 'comedy') result += Math.floor(this.performance.audience);
-    return result;
+    return Math.max(this.performance.audience - 30, 0);
   }
 }
 
@@ -44,5 +41,9 @@ class ComedyCalculator extends PerformanceCalculator {
     }
     result += 300 * this.performance.audience;
     return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
