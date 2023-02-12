573511b67dbfcfbbf2a9f2d86e490a8668f3e740
Anton Revta <anton.revta@gmail.com>
2023-02-12 16:44:46 +0300

    move amount in calculator getter

--- chapter 1/createStatementData.js
+++ chapter 1/createStatementData.js
@@ -12,7 +12,7 @@ exports.createStatementData = (invoice, plays) => {
     const calculator = new PerformanceCalculator(performance, playFor(performance));
     const result = Object.assign({}, performance);
     result.play = calculator.play;
-    result.amount = amountFor(result);
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
@@ -23,28 +23,6 @@ exports.createStatementData = (invoice, plays) => {
 
   function totalAmount(data) {
     return data.performances.reduce((sum, performance) => sum + performance.amount, 0);
-  }
-
-  function amountFor(performance) {
-    let result = 0;
-    switch (performance.play.type) {
-      case 'tragedy':
-        result = 40000;
-        if (performance.audience > 30) {
-          result += 1000 * (performance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30000;
-        if (performance.audience > 20) {
-          result += 1000 + 500 * (performance.audience - 20);
-        }
-        result += 300 * performance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${performance.play.type}`);
-    }
-    return result;
   }
 
   function volumeCreditsFor(performance) {
