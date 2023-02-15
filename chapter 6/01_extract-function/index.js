c2c123958a2513d22780228fc5f340325019194d
Anton Revta <anton.revta@gmail.com>
2023-02-15 10:22:23 +0300

    rename calculateOutstanding return value to codestyle, change outstanding to a const

--- chapter 6/01_extract-function/index.js
+++ chapter 6/01_extract-function/index.js
@@ -1,19 +1,17 @@
 const invoices = require('../../data/invoices.json');
 const printOwing = (invoice) => {
   printBanner();
-
-  let outstanding = calculateOutstanding(invoice);
-
+  const outstanding = calculateOutstanding(invoice);
   recordDueDate(invoice);
   printDetails(invoice, outstanding);
 };
 
 function calculateOutstanding(invoice) {
-  let outstanding = 0;
+  let result = 0;
   for (const o of invoice.orders) {
-    outstanding += o.amount;
+    result += o.amount;
   }
-  return outstanding;
+  return result;
 }
 
 function recordDueDate(invoice) {
