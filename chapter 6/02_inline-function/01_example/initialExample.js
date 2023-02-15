5fd70e6d37da10a099eb6436fbddeddfd532dec7
Anton Revta <anton.revta@gmail.com>
2023-02-15 11:28:13 +0300

    add initial example for inline function example1

---
+++ chapter 6/02_inline-function/01_example/initialExample.js
@@ -0,0 +1,14 @@
+const driversData = require('./drivers.json');
+function rating(driver) {
+  return moreThanFiveLateDeliveries(driver) ? 2 : 1;
+}
+
+function moreThanFiveLateDeliveries(driver) {
+  return driver.numberOfLateDeliveries > 5;
+}
+
+exports.example1 = () => {
+  const result = rating(driversData.drivers[0]);
+  console.log('\nchapter6, 02_inlineFunction, example1\n', result);
+  return result;
+};
