da596b998f48bd1d9a1067e9456e0ef0f362bddb
Anton Revta <anton.revta@gmail.com>
2023-02-16 10:50:25 +0300

    fix initial example

--- chapter 6/03_extract-variable/01_example/index.js
+++ chapter 6/03_extract-variable/01_example/index.js
@@ -3,6 +3,7 @@ const order = require('./order.json');
 function price(order) {
   //price is base price ­ quantity discount + shipping return order.quantity * order.itemPrice ­
   return (
+    order.quantity * order.itemPrice -
     Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
     Math.min(order.quantity * order.itemPrice * 0.1, 100)
   );
