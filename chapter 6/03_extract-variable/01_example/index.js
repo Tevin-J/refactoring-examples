2983e170a633ded48344d15eaf155a7e6fd96b2d
Anton Revta <anton.revta@gmail.com>
2023-02-16 10:53:26 +0300

    replace quantityDiscount in expression

--- chapter 6/03_extract-variable/01_example/index.js
+++ chapter 6/03_extract-variable/01_example/index.js
@@ -4,11 +4,7 @@ function price(order) {
   //price is base price ­ quantity discount + shipping return order.quantity * order.itemPrice ­
   const basePrice = order.quantity * order.itemPrice;
   const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
-  return (
-    basePrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(basePrice * 0.1, 100)
-  );
+  return basePrice - quantityDiscount + Math.min(basePrice * 0.1, 100);
 }
 
 exports.example1 = () => {
