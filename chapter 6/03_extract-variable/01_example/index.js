1ceb1c469c963bb65d44a33c3d5b1133d5bfe85e
Anton Revta <anton.revta@gmail.com>
2023-02-16 10:52:59 +0300

    add quantityDiscount variable

--- chapter 6/03_extract-variable/01_example/index.js
+++ chapter 6/03_extract-variable/01_example/index.js
@@ -3,6 +3,7 @@ const order = require('./order.json');
 function price(order) {
   //price is base price ­ quantity discount + shipping return order.quantity * order.itemPrice ­
   const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
   return (
     basePrice -
     Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
