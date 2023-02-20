9ae3d536b7b254593808e0c4528665eef70d0d50
Anton Revta <anton.revta@gmail.com>
2023-02-20 10:25:09 +0300

    add shipping getter

--- chapter 6/03_extract-variable/02_example/index.js
+++ chapter 6/03_extract-variable/02_example/index.js
@@ -19,8 +19,12 @@ class Order {
     return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
   }
 
+  get shipping() {
+    return Math.min(this.basePrice * 0.1, 100);
+  }
+
   get price() {
-    return this.basePrice - this.quantityDiscount + Math.min(this.basePrice * 0.1, 100);
+    return this.basePrice - this.quantityDiscount + this.shipping;
   }
 }
 
