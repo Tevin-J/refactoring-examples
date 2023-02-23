2551c282704198fc5f6294e494329fd0a4ed013e
Anton Revta <anton.revta@gmail.com>
2023-02-23 13:29:18 +0300

    update circum function name

--- chapter 6/04_change-function-declaration/01_example/index.js
+++ chapter 6/04_change-function-declaration/01_example/index.js
@@ -1,9 +1,9 @@
-function circum(radius) {
+function circumference(radius) {
   return 2 * Math.PI * radius;
 }
 
 exports.renamingFunctionSimpleMechanics = () => {
-  const result = circum(3);
+  const result = circumference(3);
   console.log(
     '\nchapter6, 04_changeFunctionDeclaration, renamingFunctionSimpleMechanics\n',
     result
