afa8bb7f8ed029e9f35fcabc5b31b35f67e3067b
Anton Revta <anton.revta@gmail.com>
2023-02-27 08:12:17 +0300

    remove usage of old function

--- chapter 6/04_change-function-declaration/03_example/index.js
+++ chapter 6/04_change-function-declaration/03_example/index.js
@@ -1,15 +1,11 @@
 const customers = require('./customers.json');
 
-function inNewEngland(customer) {
-  return xxx_inNewEngland(customer.address.state);
-}
-
 function xxx_inNewEngland(stateCode) {
   return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(stateCode);
 }
 
 exports.changingParameterToOneOfItsProperties = () => {
-  const result = customers.filter((customer) => inNewEngland(customer));
+  const result = customers.filter((customer) => xxx_inNewEngland(customer.address.state));
   console.log(
     '\nchapter6, 04_changeFunctionDeclaration, changingParameterToOneOfItsProperties\n',
     result
