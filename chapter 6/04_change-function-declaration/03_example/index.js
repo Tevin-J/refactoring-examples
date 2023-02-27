e49244e16e3e9416bd0629aeeff2239f4f9dcbf6
Anton Revta <anton.revta@gmail.com>
2023-02-27 08:12:37 +0300

    rename new function

--- chapter 6/04_change-function-declaration/03_example/index.js
+++ chapter 6/04_change-function-declaration/03_example/index.js
@@ -1,11 +1,11 @@
 const customers = require('./customers.json');
 
-function xxx_inNewEngland(stateCode) {
+function inNewEngland(stateCode) {
   return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(stateCode);
 }
 
 exports.changingParameterToOneOfItsProperties = () => {
-  const result = customers.filter((customer) => xxx_inNewEngland(customer.address.state));
+  const result = customers.filter((customer) => inNewEngland(customer.address.state));
   console.log(
     '\nchapter6, 04_changeFunctionDeclaration, changingParameterToOneOfItsProperties\n',
     result
