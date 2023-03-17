68c3c18e7db52b12582a4f7416990140da376d6e
Anton Revta <anton.revta@gmail.com>
2023-03-17 10:58:05 +0300

    add class for range

--- chapter 6/06_introduce-parameter-object/01_example/index.js
+++ chapter 6/06_introduce-parameter-object/01_example/index.js
@@ -18,6 +18,18 @@ function readingsOutsideRange(station, min, max) {
   return station.readings.filter((reading) => reading.temp < min || reading.temp > max);
 }
 
+class NumberRange {
+  constructor(min, max) {
+    this._data = { min: min, max: max };
+  }
+  get min() {
+    return this._data.min;
+  }
+  get max() {
+    return this._data.max;
+  }
+}
+
 function getResult(station, operatingPlan) {
   return readingsOutsideRange(
     station,
